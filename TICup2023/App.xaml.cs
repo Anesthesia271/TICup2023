363f1ce1f8453a40e61dd1fc464e278a5cadf6b9
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-09-21 17:18:17 +0800

    为程序添加了日志和全局异常捕获

--- TICup2023/App.xaml.cs
+++ TICup2023/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
+using NLog;
+using NLog.Config;
 
 namespace TICup2023;
 
@@ -14,5 +17,87 @@ public partial class App
         var resource = new ResourceDictionary { Source = new Uri(resStr) };
         Resources.MergedDictionaries.RemoveAt(pos);
         Resources.MergedDictionaries.Insert(pos, resource);
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        InitializeNLog();
+        RegisterEvents();
+        base.OnStartup(e);
+    }
+
+    private static void InitializeNLog()
+    {
+        var config = new LoggingConfiguration();
+        var fileTarget = new NLog.Targets.FileTarget();
+        config.AddTarget("file", fileTarget);
+        fileTarget.FileName = "${basedir}/log.txt";
+        fileTarget.Layout = @"[${date:format=HH\:mm\:ss}][${logger}] ${message}";
+        var rule = new LoggingRule("*", LogLevel.Debug, fileTarget);
+        config.LoggingRules.Add(rule);
+        LogManager.Configuration = config;
+    }
+
+    private void RegisterEvents()
+    {
+        TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        DispatcherUnhandledException += App_DispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+    }
+
+    private static void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+    {
+        try
+        {
+            if (e.Exception is Exception exception)
+            {
+                HandleException(exception);
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+        finally
+        {
+            e.SetObserved();
+        }
+    }
+
+    private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        try
+        {
+            if (e.ExceptionObject is Exception exception)
+            {
+                HandleException(exception);
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+    }
+
+    private static void App_DispatcherUnhandledException(object sender,
+        System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        try
+        {
+            HandleException(e.Exception);
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+        finally
+        {
+            e.Handled = true;
+        }
+    }
+
+    private static void HandleException(Exception ex)
+    {
+        LogManager.GetLogger("GlobalLogger").Error(ex.ToString());
     }
 }
