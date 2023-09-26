832203af71663836e59c9b77a607e5932b9d55bc
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-09-27 01:31:59 +0800

    为摄像头管理页面设置VM

---
+++ TICup2023/ViewModel/CameraContentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TICup2023.Model;
+
+namespace TICup2023.ViewModel;
+
+public partial class CameraContentViewModel : ObservableObject
+{
+    [ObservableProperty] private CameraManager _cameraManager = CameraManager.GetInstance();
+    
+    [RelayCommand]
+    private async Task UpdateCameraDevicesAsync()
+    {
+        await Task.Run(() => CameraManager.UpdateCameraDevices());
+        OnPropertyChanged(nameof(CameraManager));
+    }
+    
+    [RelayCommand]
+    private void OpenCamera()
+    {
+        CameraManager.OpenCamera();
+    }
+}
