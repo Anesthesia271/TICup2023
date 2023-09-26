a415d39b3f733196205eae1925cc050d4a831f9e
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-09-27 01:24:52 +0800

    实现主界面右侧父控件

--- TICup2023/UserControl/MainContent.xaml.cs
+++ TICup2023/UserControl/MainContent.xaml.cs
@@ -1,5 +1,3 @@
-using System.Windows.Controls;
-
 namespace TICup2023.UserControl;
 
 public partial class MainContent
---
+++ TICup2023/ViewModel/MainContentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TICup2023.UserControl;
+
+namespace TICup2023.ViewModel;
+
+public partial class MainContentViewModel : ObservableObject
+{
+    private CameraDebugContent CameraDebugContent { get; } = new();
+    private SynthesisMatchContent SynthesisMatchContent { get; } = new();
+
+    [ObservableProperty] private string _contentTitle = "视频调试";
+    [ObservableProperty] private object _subContent;
+
+    public MainContentViewModel()
+    {
+        SubContent = CameraDebugContent;
+    }
+
+    [RelayCommand]
+    private void Switch()
+    {
+        if (SubContent is CameraDebugContent)
+        {
+            ContentTitle = "综合";
+            SubContent = SynthesisMatchContent;
+        }
+        else
+        {
+            ContentTitle = "视频调试";
+            SubContent = CameraDebugContent;
+        }
+    }
+}
