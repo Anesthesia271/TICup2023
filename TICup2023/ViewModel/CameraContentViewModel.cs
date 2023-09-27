e2f6c8b5d1ac2745fcbeb1127cc2e977960e6dc8
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-09-28 00:21:52 +0800

    添加颜色识别参数设置，为打开关闭摄像头操作添加安全措施

--- TICup2023/ViewModel/CameraContentViewModel.cs
+++ TICup2023/ViewModel/CameraContentViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using HandyControl.Controls;
 using TICup2023.Model;
 
 namespace TICup2023.ViewModel;
@@ -8,6 +10,7 @@ namespace TICup2023.ViewModel;
 public partial class CameraContentViewModel : ObservableObject
 {
     [ObservableProperty] private CameraManager _cameraManager = CameraManager.GetInstance();
+    [ObservableProperty] private bool _isCameraStateChanging;
     
     [RelayCommand]
     private async Task UpdateCameraDevicesAsync()
@@ -17,8 +20,26 @@ public partial class CameraContentViewModel : ObservableObject
     }
     
     [RelayCommand]
-    private void OpenCamera()
+    private async Task OpenCameraAsync()
     {
-        CameraManager.OpenCamera();
+        try
+        {
+            await Task.Run(() => CameraManager.OpenCamera());
+            OnPropertyChanged(nameof(CameraManager));
+            IsCameraStateChanging = false;
+        }
+        catch (Exception e)
+        {
+            Growl.Warning($"打开摄像头失败，异常信息为：{e.Message}");
+            IsCameraStateChanging = false;
+        }
+    }
+    
+    [RelayCommand]
+    private async Task CloseCameraAsync()
+    {
+        await Task.Run(() => CameraManager.CloseCamera());
+        OnPropertyChanged(nameof(CameraManager));
+        IsCameraStateChanging = false;
     }
 }
