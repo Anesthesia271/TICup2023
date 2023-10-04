babd4a1e10ba094d287653b3b0b56f71f7e45fc2
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-10-04 20:48:29 +0800

    添加MultiIntConverter类

---
+++ TICup2023/Tool/Converter/MultiIntConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TICup2023.Tool.Converter;
+
+public class MultiIntConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null || values.Length == 0)
+            throw new ArgumentNullException(nameof(values));
+        return (int)values[0] == (int)values[1] ? values[0] : -1;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return new[] { value, value };
+    }
+}
