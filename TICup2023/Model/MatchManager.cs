dd00069e1cae84bcde6c9ad37e329cb1c073fe5a
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-10-04 20:51:13 +0800

    为MatchManager类添加一些基础内容

---
+++ TICup2023/Model/MatchManager.cs
@@ -0,0 +1,11 @@
+﻿namespace TICup2023.Model;
+
+public class MatchManager
+{
+    private static readonly MatchManager Instance = new();
+    
+    public int[] MapSizeList { get; } = {8, 10};
+    public int MapSize { get; set; } = 8;
+    
+    public static MatchManager GetInstance() => Instance;
+}
