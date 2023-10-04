71787b5a18a0cefa83bbc7ec47236c76e7c63328
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-10-04 20:51:47 +0800

    配置MatchContent和其ViewModel

--- TICup2023/ViewModel/MatchContentViewModel.cs
+++ TICup2023/ViewModel/MatchContentViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using TICup2023.Model;
 
 namespace TICup2023.ViewModel;
 
 public partial class MatchContentViewModel : ObservableObject
 {
+    [ObservableProperty] private MatchManager _matchManager = MatchManager.GetInstance();
+    [ObservableProperty] private CameraManager _cameraManager = CameraManager.GetInstance();
 }
