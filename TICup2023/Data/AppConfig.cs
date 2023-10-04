27a6853e10a2461e7fbb7b869d1c2f6cbcae0afc
聂凡 <nief21@mails.tsinghua.edu.cn>
2023-10-04 20:42:21 +0800

    添加用于保存配置的类

---
+++ TICup2023/Data/AppConfig.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TICup2023.Data;
+
+internal class AppConfig
+{
+    public static readonly string SavePath = $"{AppDomain.CurrentDomain.BaseDirectory}Config.json";
+    
+    
+}
